089a1b973255bbc3e7dd5c8e54defdb23fddc00c
Aziz Rakhim <azizrakhim17@gmail.com>
2024-08-06 11:20:09 +0500

    prevent duplicate items

--- script.js
+++ script.js
@@ -32,6 +32,11 @@ function onAddItemSubmit(e) {
     itemToEdit.classList.remove("edit-mode");
     itemToEdit.remove();
     isEditMode = false;
+  } else {
+    if (checkIfItemExists(newItem)) {
+      alert("That item already exists!");
+      return;
+    }
   }
 
   // Create item DOM element
@@ -100,6 +105,11 @@ function onClickItem(e) {
   } else {
     setItemToEdit(e.target);
   }
+}
+
+function checkIfItemExists(item) {
+  const itemsFromStorage = getItemsFromStorage();
+  return itemsFromStorage.includes(item);
 }
 
 function setItemToEdit(item) {
